7bb327c7d6f23e8a23a32a842c0947a8e19a2592
Antonin Hildebrand <antonin@hildebrand.cz>
2016-10-23 20:57:16 +0200

    prove that :closure-output-charset is respected

--- src/closure_output_charset/core.cljs
+++ src/closure_output_charset/core.cljs
@@ -1,6 +1,7 @@
 (ns closure-output-charset.core
   (:require [clojure.browser.repl :as repl]
-            [cljsjs.vega]))
+            [cljsjs.vega]
+            [goog.i18n.bidi :as bidi]))
 
 ;; (defonce conn
 ;;   (repl/connect "http://localhost:9000/repl"))
@@ -8,3 +9,5 @@
 (enable-console-print!)
 
 (println "Hello world!")
+
+(println "!" bidi/rtlChars_)                                                                                                  ; see https://github.com/google/closure-compiler/issues/1704
