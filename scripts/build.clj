a302d62fa65072ba72af6896856b62028664b12e
Antonin Hildebrand <antonin@hildebrand.cz>
2016-10-23 20:56:49 +0200

    clean individual build target folders just before building

--- scripts/build.clj
+++ scripts/build.clj
@@ -1,25 +1,30 @@
-(require '[cljs.build.api :as b])
+(require '[cljs.build.api :as b]
+         '[clojure.java.shell :refer [sh]])
 
 (println "Building ...")
 
 (let [start (System/nanoTime)]
+  (sh "rm" "-rf" "out/dev_utf8")
   (b/build "src"
            {:main       'closure-output-charset.core
             :output-to  "out/dev_utf8/closure_output_charset.js"
             :output-dir "out/dev_utf8"
             :verbose    true})
+  (sh "rm" "-rf" "out/dev_ascii")
   (b/build "src"
            {:main                   'closure-output-charset.core
             :output-to              "out/dev_ascii/closure_output_charset.js"
             :output-dir             "out/dev_ascii"
             :closure-output-charset "US-ASCII"
             :verbose                true})
+  (sh "rm" "-rf" "out/adv_utf8")
   (b/build "src"
            {:main          'closure-output-charset.core
             :output-to     "out/adv_utf8/closure_output_charset.js"
             :output-dir    "out/adv_utf8"
             :optimizations :advanced
             :verbose       true})
+  (sh "rm" "-rf" "out/adv_ascii")
   (b/build "src"
            {:main                   'closure-output-charset.core
             :output-to              "out/adv_ascii/closure_output_charset.js"
@@ -28,5 +33,3 @@
             :closure-output-charset "US-ASCII"
             :verbose                true})
   (println "... done. Elapsed" (/ (- (System/nanoTime) start) 1e9) "seconds"))
-
-
